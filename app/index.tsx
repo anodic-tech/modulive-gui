b334c9915ab5b8b2e0a2f74d59069cadc149569f
nodemon <bernelectronic@gmail.com>
2024-07-22 17:48:24 -0400

    Initial GUI

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+import useSocket from "@/hooks/useSocket";
+
+export default function Index() {
+
+  const data = useSocket()
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>{data}</Text>
+    </View>
+  );
+}
---
+++ hooks/useSocket.ts
@@ -0,0 +1,22 @@
+import dgram from 'react-native-udp';
+import { useEffect, useState } from 'react';
+
+const PORT = Number(process.env.EXPO_PUBLIC_SOCKET_PORT)
+const ADDRESS = process.env.EXPO_PUBLIC_SOCKET_ADDRESS
+
+export default () => {
+
+    const [data, setData] = useState('None');
+  
+    useEffect(() => {
+        const socket = dgram.createSocket({type:'udp4', debug: true});
+        socket.bind({port: PORT, address: ADDRESS});
+        socket.on('message', function(msg, _) {
+            const newData = JSON.parse(msg.toString()).data;
+            console.info('Message received', newData);
+            setData(newData);
+          })
+        return () => { socket.close(); }
+    },[PORT,ADDRESS]);
+    return data;
+};
